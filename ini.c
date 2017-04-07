6f0c3faffa2c8aacaf237978bd901069edea3ea6
tigrazone <tigrazone@gmail.com>
2017-04-07 17:52:02 +0300

    Add files via upload

--- ini.c
+++ ini.c
@@ -96,12 +96,6 @@ int ini_parse_stream(ini_reader reader, void* stream, ini_handler handler,
     }
 #endif
 
-#if INI_HANDLER_LINENO
-#define HANDLER(u, s, n, v) handler(u, s, n, v, lineno)
-#else
-#define HANDLER(u, s, n, v) handler(u, s, n, v)
-#endif
-
     /* Scan through stream line by line */
     while (reader(line, INI_MAX_LINE, stream) != NULL) {
         lineno++;
@@ -124,7 +118,7 @@ int ini_parse_stream(ini_reader reader, void* stream, ini_handler handler,
         else if (*prev_name && *start && start > line) {
             /* Non-blank line with leading whitespace, treat as continuation
                of previous name's value (as per Python configparser). */
-            if (!HANDLER(user, section, prev_name, start) && !error)
+            if (!handler(user, section, prev_name, start) && !error)
                 error = lineno;
         }
 #endif
@@ -147,18 +141,17 @@ int ini_parse_stream(ini_reader reader, void* stream, ini_handler handler,
             if (*end == '=' || *end == ':') {
                 *end = '\0';
                 name = rstrip(start);
-                value = end + 1;
+                value = lskip(end + 1);
 #if INI_ALLOW_INLINE_COMMENTS
                 end = find_chars_or_comment(value, NULL);
                 if (*end)
                     *end = '\0';
 #endif
-                value = lskip(value);
                 rstrip(value);
 
                 /* Valid name[=:]value pair found, call handler */
                 strncpy0(prev_name, name, sizeof(prev_name));
-                if (!HANDLER(user, section, name, value) && !error)
+                if (!handler(user, section, name, value) && !error)
                     error = lineno;
             }
             else if (!error) {
@@ -195,6 +188,10 @@ int ini_parse(const char* filename, ini_handler handler, void* user)
     file = fopen(filename, "r");
     if (!file)
         return -1;
+	
+	//tigra: можно буферизировать
+	setvbuf(file, NULL, _IOFBF, 128*1024 );
+	
     error = ini_parse_file(file, handler, user);
     fclose(file);
     return error;
